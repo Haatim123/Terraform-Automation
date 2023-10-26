b58aa04ff2e81ac7a10ef5aa44b03923a1ccee92
Haatim123 <98979131+Haatim123@users.noreply.github.com>
2023-10-26 13:03:49 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "mydev-tf-state-bucket-project-terraform-batch-20-hatim"
     key = "main"
-    region = "us-east-2"
+    region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
